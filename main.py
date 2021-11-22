cd3c5f06afe7b024fefa2c9d1466bb7edf632b12
Nilesh <nilesh.acharekar@gmail.com>
2021-11-23 00:32:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from wsgiref import simple_server
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    return "Flask app is running and I am changing something"
+
+port = int(os.getenv("PORT", 5001))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    # app.run()
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku-deployment",
+    version="0.0.1",
+    description="ML project",
+    author="Nilesh Acharekar",
+    packages=find_packages(),
+    license="MIT"
+)
