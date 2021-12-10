b68ba1835a55cbbd08fed3974fbe64f57922eea2
Nilesh <nilesh.acharekar@gmail.com>
2021-12-10 20:15:12 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docker-heroku-deployment",
+    name="wafer-docker-heroku-deployment",
     version="0.0.1",
     description="ML project",
     author="Nilesh Acharekar",
